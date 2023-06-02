bede6621ae4dc4cac9d62ab3978314eb66cce42b
TomyRulliat <128361624+TomyRulliat@users.noreply.github.com>
2023-06-02 15:25:11 +0200

    Add files via upload

---
+++ Sites_WEB/Projet4-ElectronicArts/EA.js
@@ -0,0 +1,30 @@
+/* Fonction inutile en raison d'un changement de l'animation du home_screen 
+
+function home_screen() {
+    document.getElementById("left").style.transform = "translateX(-60vw)";
+    document.getElementById("rond").style.transform = "translateX(-60vw)";
+    document .getElementById("ligne").style.transform = "translateX(-60vw)";
+    document.getElementById("logo_home").style.transform = "translateX(-60vw)";
+    document.getElementById("right").style.transform = "translateX(60vw)";
+}
+*/
+
+function img1() {
+    document.getElementById("carroussel").style.animation = "img1 2s forwards";
+}
+
+function img2() {
+    document.getElementById("carroussel").style.animation = "img2 2s forwards";
+}
+
+function img3() {
+    document.getElementById("carroussel").style.animation = "img3 2s forwards";
+}
+
+function img4() {
+    document.getElementById("carroussel").style.animation = "img4 2s forwards";
+}
+
+function img5() {
+    document.getElementById("carroussel").style.animation = "img5 2s forwards";
+}
